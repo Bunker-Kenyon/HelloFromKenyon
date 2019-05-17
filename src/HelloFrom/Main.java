e260407c31f3415f34e79d35c74eed8c381e4301
Kenyon Bunker <kjbunker817@gmail.com>
2019-05-16 19:49:56 -0700

    Made additional changes to FeatureA

--- src/HelloFrom/Main.java
+++ src/HelloFrom/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello, my name is Kenyon.");
         System.out.println("Hello Kenyon, my name is Adrian.");
         System.out.println("Added FeatureA Branch");
+        System.out.println("Made additional changes to FeatureA");
     }
 }
