da0728471b8ec827b11e3f3b2897cc7e10829202
Kenyon Bunker <kjbunker817@gmail.com>
2019-05-16 19:47:18 -0700

    Created FeatureA Branch
    Made changes to code

--- src/HelloFrom/Main.java
+++ src/HelloFrom/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Kenyon.");
         System.out.println("Hello Kenyon, my name is Adrian.");
+        System.out.println("Added FeatureA Branch");
     }
 }
